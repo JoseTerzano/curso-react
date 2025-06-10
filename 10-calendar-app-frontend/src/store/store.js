3bcb9ded813b3be47e45754af56a5ba6942b0a90
Jose <joseterzano@gmail.com>
2025-06-09 23:29:08 -0300

    listo

---
+++ 10-calendar-app-frontend/src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { calendarSlice } from './calendar/CalendarSlice';
+import { uiSlice } from './ui/uiSlice';
+
+
+
+export const store = configureStore({
+    reducer: {
+        calendar: calendarSlice.reducer,
+        ui: uiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+})
